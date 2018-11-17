61ffad5291b6ca9bb9d4f70c4f8085ba27a39746
Richard <richard.k.loveland@gmail.com>
2018-11-17 09:45:40 -0700

    Integration of Everything but email parser

--- ojo.sh
+++ ojo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 while true;
 do  echo "Getting Fridge Snapshot"
 ffmpeg -y -f video4linux2 -i /dev/video1 -vframes 1 fridge.jpg
 echo "Calculating OJ Percent"
-./get_oj_percent.py fridge.jpg
-sleep 5m
+python get_oj_percent.py fridge.jpg
+sleep 20s
 done
